640cebce5364c6186d25caa23b4056ce57282cc9
ethanscottrudy <ethanscottrudy@gmail.com>
2025-10-14 18:49:55 -0700

    deleted file, added button and js

---
+++ exercises/ex04.js
@@ -0,0 +1,21 @@
+// add a hutton titled click me
+
+//measure total number
+let count = 0;
+$("#needy-button").html("hello");
+$("#needy-button").click( function() {
+    count = count + 3;
+    $("#needy-button").html("You Clicked Me " + count + " times");
+});
+//When it it clicked
+// add one to click count
+
+//show how many times
+// on our button
+// show "clicked total nmber times"
+
+
+// top limit
+
+//
+
